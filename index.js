25130926cc5e7d03eafc9445ae1ca19ee03b0299
Emily Chen <emilycodeee@gmail.com>
2021-12-01 00:07:11 +0800

    Fix image dismiss

--- index.js
+++ index.js
@@ -4,219 +4,185 @@ const GAME_STATE = {
   CardsMatchFailed: "CardsMatchFailed",
   CardsMatched: "CardsMatched",
   GameFinished: "GameFinished",
-}
-
-
-
-
-// 0  0-12：黑桃 1-13
-// 1  13 - 25：愛心 1 - 13
-// 2  26 - 38：方塊 1 - 13
-// 3  39 - 51：梅花 1 - 13
+};
 
 const Symbols = [
-  'https://image.flaticon.com/icons/svg/105/105223.svg', // 黑桃
-  'https://image.flaticon.com/icons/svg/105/105220.svg', // 愛心
-  'https://image.flaticon.com/icons/svg/105/105212.svg', // 方塊
-  'https://image.flaticon.com/icons/svg/105/105219.svg' // 梅花
-]
+  "./images/spades.png",
+  "./images/heart.png",
+  "./images/diamond.png",
+  "./images/clover.png",
+];
 
 const view = {
-  // displayCards: function displayCards() { ...  }
-
   getCardElement(index) {
-
-    return `    <div data-index="${index}"class="card back"></div>`
+    return `    <div data-index="${index}"class="card back"></div>`;
   },
 
   getCardContent(index) {
-
-    const number = this.transformNumber((index % 13) + 1)
-    const symbol = Symbols[Math.floor(index / 13)]
+    const number = this.transformNumber((index % 13) + 1);
+    const symbol = Symbols[Math.floor(index / 13)];
     return `   
 
       <p>${number}</p>
       <img src="${symbol}" alt="">
       <p>${number}</p>
 
-  `
+  `;
+  },
 
-  }
-
-  ,
   transformNumber(number) {
     switch (number) {
       case 1:
-        return 'A'
+        return "A";
       case 11:
-        return 'J'
+        return "J";
       case 12:
-        return 'Q'
+        return "Q";
       case 13:
-        return 'K'
+        return "K";
       default:
-        return number
+        return number;
     }
   },
 
   displayCards(indexes) {
-    const rootElement = document.querySelector('#cards')
-    rootElement.innerHTML = indexes.map(index => this.getCardElement(index)).join('')
+    const rootElement = document.querySelector("#cards");
+    rootElement.innerHTML = indexes
+      .map((index) => this.getCardElement(index))
+      .join("");
   },
-  // flipCard(card)
-  // flipCards(1,2,3,4,5)
-  // cards = [1,2,3,4,5]
-
 
   flipCards(...cards) {
-    cards.map(card => {
-      if (card.classList.contains('back')) {
-        card.classList.remove('back')
-        card.innerHTML = this.getCardContent(Number(card.dataset.index))
-        return
+    cards.map((card) => {
+      if (card.classList.contains("back")) {
+        card.classList.remove("back");
+        card.innerHTML = this.getCardContent(Number(card.dataset.index));
+        return;
       }
-      card.classList.add('back')
-      card.innerHTML = null
-
-    })
-
+      card.classList.add("back");
+      card.innerHTML = null;
+    });
   },
 
   pairCards(...cards) {
-    cards.map(card => {
-      card.classList.add('paired')
-    })
-
+    cards.map((card) => {
+      card.classList.add("paired");
+    });
   },
 
   renderScore(score) {
-    document.querySelector('.score').innerText = `Score: ${score}`
+    document.querySelector(".score").innerText = `Score: ${score}`;
   },
   renderTriedTimes(times) {
-    document.querySelector('.tried').innerText = `You've tried: ${times}`
+    document.querySelector(".tried").innerText = `You've tried: ${times}`;
   },
   appendWrongAnimation(...cards) {
-    cards.map(card => {
-      card.classList.add('wrong')
-      card.addEventListener('animationend', event => event.target.classList.remove('wrong'), { once: true })
-    })
+    cards.map((card) => {
+      card.classList.add("wrong");
+      card.addEventListener(
+        "animationend",
+        (event) => event.target.classList.remove("wrong"),
+        { once: true }
+      );
+    });
   },
   showGameFinished() {
-    const div = document.createElement('div')
-    div.classList.add('completed')
+    const div = document.createElement("div");
+    div.classList.add("completed");
     div.innerHTML = `
       <p>Complete!</p>
       <p>Score: ${model.score}</p>
       <p>You've tried: ${model.triedTimes} times</p>
-    `
-    const header = document.querySelector('#header')
-    header.before(div)
-  }
-
-}
+    `;
+    const header = document.querySelector("#header");
+    header.before(div);
+  },
+};
 
 const utility = {
   getRandimNumberArray(count) {
-    // count = 5 => [2,3,4,1,0]
-    const number = Array.from(Array(count).keys())
+    const number = Array.from(Array(count).keys());
     for (let index = number.length - 1; index > 0; index--) {
       let randomIndex = Math.floor(Math.random() * (index + 1));
-      // [1,2,3,4,5] 1 5交換
-      // const temp = 1
-      // arr[4] = temp
-      // arr[0] = 5
 
-      [number[index], number[randomIndex]] = [number[randomIndex], number[index]]
-
+      [number[index], number[randomIndex]] = [
+        number[randomIndex],
+        number[index],
+      ];
     }
 
-    return number
-  }
-
-}
+    return number;
+  },
+};
 
 const controller = {
   currentState: GAME_STATE.FirstCardAwaits,
 
-
   generateCards() {
-    view.displayCards(utility.getRandimNumberArray(52))
+    view.displayCards(utility.getRandimNumberArray(52));
   },
-  // 依照不同遊戲狀態，做不同的行為
   dispatchCardAction(card) {
-    if (!card.classList.contains('back')) {
-      return
+    if (!card.classList.contains("back")) {
+      return;
     }
 
     switch (this.currentState) {
       case GAME_STATE.FirstCardAwaits:
-        view.flipCards(card)
-        model.revealedCards.push(card)
-        this.currentState = GAME_STATE.SecondCardAwaits
-        break
+        view.flipCards(card);
+        model.revealedCards.push(card);
+        this.currentState = GAME_STATE.SecondCardAwaits;
+        break;
 
       case GAME_STATE.SecondCardAwaits:
-        view.renderTriedTimes(++model.triedTimes)
-        view.flipCards(card)
-        model.revealedCards.push(card)
-
+        view.renderTriedTimes(++model.triedTimes);
+        view.flipCards(card);
+        model.revealedCards.push(card);
 
         if (model.isRevealedCardsMatched()) {
-          // 配對正確
-          view.renderScore(model.score += 10)
-          this.currentState = GAME_STATE.CardsMatched
-          view.pairCards(...model.revealedCards)
-          model.revealedCards = []
+          view.renderScore((model.score += 10));
+          this.currentState = GAME_STATE.CardsMatched;
+          view.pairCards(...model.revealedCards);
+          model.revealedCards = [];
           if (model.score === 260) {
-            console.log('showGameFinished')
-            this.currentState = GAME_STATE.GameFinished
-            view.showGameFinished()  // 加在這裡
-            return
+            console.log("showGameFinished");
+            this.currentState = GAME_STATE.GameFinished;
+            view.showGameFinished();
+            return;
           }
-          this.currentState = GAME_STATE.FirstCardAwaits
+          this.currentState = GAME_STATE.FirstCardAwaits;
         } else {
-          // 配對失敗
-          this.currentState = GAME_STATE.CardsMatchFailed
-          view.appendWrongAnimation(...model.revealedCards)
-          setTimeout(this.resetCards, 1000)
-          // setTimeout的第一個參數為函式本身 故不加括號
-
+          this.currentState = GAME_STATE.CardsMatchFailed;
+          view.appendWrongAnimation(...model.revealedCards);
+          setTimeout(this.resetCards, 1000);
         }
-        break
+        break;
     }
-
-    console.log('current:', this.currentState)
-    // console.log('reveled:', model.revealedCards.map(card => card.dataset.index))
-
-    console.log('revel:', model.revealedCards.map(card => card.dataset.index))
   },
 
   resetCards() {
-    view.flipCards(...model.revealedCards)
-    model.revealedCards = []
-    controller.currentState = GAME_STATE.FirstCardAwaits
-  }
-
-}
+    view.flipCards(...model.revealedCards);
+    model.revealedCards = [];
+    controller.currentState = GAME_STATE.FirstCardAwaits;
+  },
+};
 
 const model = {
   revealedCards: [],
 
   isRevealedCardsMatched() {
-    return this.revealedCards[0].dataset.index % 13 === this.revealedCards[1].dataset.index % 13
-
+    return (
+      this.revealedCards[0].dataset.index % 13 ===
+      this.revealedCards[1].dataset.index % 13
+    );
   },
   score: 0,
-  triedTimes: 0
+  triedTimes: 0,
+};
 
+controller.generateCards();
 
-}
-
-controller.generateCards()
-
-
-document.querySelectorAll('.card').forEach(card => {
-  card.addEventListener('click', event => {
-    controller.dispatchCardAction(card)
-  })
-})
+document.querySelectorAll(".card").forEach((card) => {
+  card.addEventListener("click", (event) => {
+    controller.dispatchCardAction(card);
+  });
+});
